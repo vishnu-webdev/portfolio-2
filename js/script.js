f407a8d68c45bad3544a3e45dbce1ad2aa035186
vishnu-webdev <mail.vishnunm@gmail.com>
2026-07-21 20:17:54 +0530

    added hamburger and more project card

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('navLinks');
+
+if (hamburger && navLinks) {
+    hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+        hamburger.classList.toggle('active');
+    });
+}
